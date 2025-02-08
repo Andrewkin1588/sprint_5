ab68977317d90a4e5e8b1b99ee89af4e22656835
Andrey Busyrev <drewsbus@yandex.ru>
2025-02-08 11:30:59 +0500

    Добавил фикстуры

--- conftest.py
+++ conftest.py
@@ -2,9 +2,12 @@ import random
 
 import pytest
 from faker import Faker
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+from locators import LogInMainPageLocators, LogInPage, PageRegistration
 
 r = random.Random()
 faker = Faker()
@@ -41,3 +44,37 @@ class Helpers:
         """
         return WebDriverWait(driver, 5).until(
             EC.visibility_of_element_located((By.XPATH, locator)))
+
+
+@pytest.fixture(scope='function')
+def registration(driver, helpers):
+    # Переходим на главную страницу
+    url = 'https://stellarburgers.nomoreparties.site/'
+    driver.get(url)
+
+    # Ожидаем появлния кнопки "Войти в аккаунт"
+    helpers.wait_until_visibility_of_element_located(driver, LogInMainPageLocators.LOG_IN_BUTTON)
+
+    # Нажимаем на кнопку "Войти в аккаунт"
+    driver.find_element(By.XPATH, LogInMainPageLocators.LOG_IN_BUTTON).click()
+
+    # Ожидаем появления ссылки "Зарегистрироваться"
+    helpers.wait_until_visibility_of_element_located(driver, LogInPage.REGISTRATION_LINK)
+
+    # Нажимаем на линк "Зарегистрироваться"
+    driver.find_element(By.XPATH, LogInPage.REGISTRATION_LINK).click()
+
+    # Ожидаем появления полей для ввода
+    helpers.wait_until_visibility_of_element_located(driver, PageRegistration.INPUT_NAME_EMAIL_PASSWORD)
+
+
+@pytest.fixture(scope='function')
+def driver():
+    driver = webdriver.Chrome()
+    return driver
+
+
+@pytest.fixture(scope='function')
+def helpers():
+    helper = Helpers()
+    return helper
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,85 +1,44 @@
-import time
-
-from selenium import webdriver
 from selenium.webdriver.common.by import By
-from conftest import Helpers, GenerateTestData
-from locators import LogInMainPageLocators, PageRegistration, LogInPage
+from conftest import GenerateTestData
+from locators import PageRegistration, LogInPage
 
-driver = webdriver.Chrome()
-helper = Helpers()
+
 generator = GenerateTestData()
 
 
-def test_registration_positive():
-    # Переходим на главную страницу
-    url = 'https://stellarburgers.nomoreparties.site/'
-    driver.get(url)
+class TestRegistration:
+    def test_registration_positive(self, registration, driver, helpers):
+        # Заполняем поля для регистрации
+        fields_input = driver.find_elements(By.XPATH, PageRegistration.INPUT_NAME_EMAIL_PASSWORD)
+        fields_input[0].send_keys(generator.generate_name())
+        fields_input[1].send_keys(generator.generate_email())
+        fields_input[2].send_keys(generator.generate_password())
 
-    # Ожидаем появлния кнопки "Войти в аккаунт"
-    helper.wait_until_visibility_of_element_located(driver, LogInMainPageLocators.LOG_IN_BUTTON)
+        # Нажимаем кнопку "Зарегистрироваться"
+        driver.find_element(By.XPATH, PageRegistration.REGISTRATION_BUTTON).click()
 
-    # Нажимаем на кнопку "Войти в аккаунт"
-    driver.find_element(By.XPATH, LogInMainPageLocators.LOG_IN_BUTTON).click()
+        # Ожидаем появления кнопки "Войти" после успешной регистрации
+        helpers.wait_until_visibility_of_element_located(driver, LogInPage.LOG_IN_BUTTON)
 
-    # Ожидаем появления ссылки "Зарегистрироваться"
-    helper.wait_until_visibility_of_element_located(driver, LogInPage.REGISTRATION_LINK)
+        # Закрываем браузер
+        driver.quit()
 
-    # Нажимаем на линк "Зарегистрироваться"
-    driver.find_element(By.XPATH, LogInPage.REGISTRATION_LINK).click()
+    def test_registration_negative(self, registration, driver, helpers):
+        # Заполняем поля для регистрации
+        fields_input = driver.find_elements(By.XPATH, PageRegistration.INPUT_NAME_EMAIL_PASSWORD)
+        fields_input[0].send_keys(generator.generate_name())
+        fields_input[1].send_keys(generator.generate_email())
+        fields_input[2].send_keys(generator.generate_password_negative())
 
-    # Ожидаем появления полей для ввода
-    helper.wait_until_visibility_of_element_located(driver, PageRegistration.INPUT_NAME_EMAIL_PASSWORD)
+        # Нажимаем кнопку "Зарегистрироваться"
+        driver.find_element(By.XPATH, PageRegistration.REGISTRATION_BUTTON).click()
 
-    # Заполняем поля для регистрации
-    fields_input = driver.find_elements(By.XPATH, PageRegistration.INPUT_NAME_EMAIL_PASSWORD)
-    fields_input[0].send_keys(generator.generate_name())
-    fields_input[1].send_keys(generator.generate_email())
-    fields_input[2].send_keys(generator.generate_password())
+        # Ожидаем появления ошибки "Некорректный пароль" т.к. кол-во символов меньше чем должно быть
+        helpers.wait_until_visibility_of_element_located(driver, PageRegistration.INVALID_PASSWORD_ERROR)
 
-    # Нажимаем кнопку "Зарегистрироваться"
-    driver.find_element(By.XPATH, PageRegistration.REGISTRATION_BUTTON).click()
+        # Проверяем текст сообщения о некорректном пароле
+        invalid_password_text = driver.find_element(By.XPATH, PageRegistration.INVALID_PASSWORD_ERROR).text
+        assert invalid_password_text == 'Некорректный пароль'
 
-    # Ожидаем появления кнопки "Войти" после успешной регистрации
-    helper.wait_until_visibility_of_element_located(driver, LogInPage.LOG_IN_BUTTON)
-
-    # Закрываем браузер
-    driver.quit()
-
-def test_registration_negative():
-    # Переходим на главную страницу
-    url = 'https://stellarburgers.nomoreparties.site/'
-    driver.get(url)
-
-    # Ожидаем появлния кнопки "Войти в аккаунт"
-    helper.wait_until_visibility_of_element_located(driver, LogInMainPageLocators.LOG_IN_BUTTON)
-
-    # Нажимаем на кнопку "Войти в аккаунт"
-    driver.find_element(By.XPATH, LogInMainPageLocators.LOG_IN_BUTTON).click()
-
-    # Ожидаем появления ссылки "Зарегистрироваться"
-    helper.wait_until_visibility_of_element_located(driver, LogInPage.REGISTRATION_LINK)
-
-    # Нажимаем на линк "Зарегистрироваться"
-    driver.find_element(By.XPATH, LogInPage.REGISTRATION_LINK).click()
-
-    # Ожидаем появления полей для ввода
-    helper.wait_until_visibility_of_element_located(driver, PageRegistration.INPUT_NAME_EMAIL_PASSWORD)
-
-    # Заполняем поля для регистрации
-    fields_input = driver.find_elements(By.XPATH, PageRegistration.INPUT_NAME_EMAIL_PASSWORD)
-    fields_input[0].send_keys(generator.generate_name())
-    fields_input[1].send_keys(generator.generate_email())
-    fields_input[2].send_keys(generator.generate_password_negative())
-
-    # Нажимаем кнопку "Зарегистрироваться"
-    driver.find_element(By.XPATH, PageRegistration.REGISTRATION_BUTTON).click()
-
-    # Ожидаем появления ошибки "Некорректный пароль" т.к. кол-во символов меньше чем должно быть
-    helper.wait_until_visibility_of_element_located(driver, PageRegistration.INVALID_PASSWORD_ERROR)
-
-    # Проверяем текст сообщения о некорректном пароле
-    invalid_password_text = driver.find_element(By.XPATH, PageRegistration.INVALID_PASSWORD_ERROR).text
-    assert invalid_password_text == 'Некорректный пароль'
-
-    # Закрываем браузер
-    driver.quit()
+        # Закрываем браузер
+        driver.quit()
